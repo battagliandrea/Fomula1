3d8eeeabae982b74a257c811404c68e763d572fe
Andrea Battaglia <battaglia.andry@gmail.com>
2023-11-05 00:02:46 +0100

    feat: implement results di

--- data/results/results-di/build.gradle.kts
+++ data/results/results-di/build.gradle.kts
@@ -10,4 +10,12 @@ android {
 dependencies {
     api(projects.data.results.resultsApi)
     api(projects.data.results.resultsImpl)
+
+    implementation(projects.core.network.networkApi)
+
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.serialization)
 }
---
+++ data/results/results-di/src/main/kotlin/it/battagliandrea/formula1/data/results/di/NetworkModule.kt
@@ -0,0 +1,42 @@
+package it.battagliandrea.formula1.data.results.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import it.battagliandrea.formula1.data.results.api.repository.IResultsRepository
+import it.battagliandrea.formula1.data.results.impl.network.ErgastApiContract
+import it.battagliandrea.formula1.data.results.impl.repository.ResultsRepository
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.Call
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface NetworkModule {
+
+    @Binds
+    @Singleton
+    fun bindRepository(repository: ResultsRepository): IResultsRepository
+
+    companion object {
+        @OptIn(ExperimentalSerializationApi::class)
+        @Provides
+        @Singleton
+        fun provideRetorif(json: Json, callFactory: Call.Factory) =
+            Retrofit.Builder()
+                .baseUrl(it.battagliandrea.formula1.core.network.api.ErgastBaseUrl)
+                .callFactory(callFactory)
+                .addConverterFactory(
+                    json.asConverterFactory("application/json".toMediaType()),
+                )
+                .build()
+                .create(ErgastApiContract::class.java)
+    }
+
+}
