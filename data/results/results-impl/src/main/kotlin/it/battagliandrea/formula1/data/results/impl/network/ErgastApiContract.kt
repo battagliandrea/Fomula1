c753b880a7ff1715176a82e5fa48038b5695bcd5
Andrea Battaglia <battaglia.andry@gmail.com>
2023-11-05 00:01:40 +0100

    feat: define results api contract

---
+++ data/results/results-impl/src/main/kotlin/it/battagliandrea/formula1/data/results/impl/network/ErgastApiContract.kt
@@ -0,0 +1,23 @@
+package it.battagliandrea.formula1.data.results.impl.network
+
+import it.battagliandrea.formula1.data.results.impl.models.tables.BaseResponse
+import it.battagliandrea.formula1.data.results.impl.models.tables.DataRaceTableDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ErgastApiContract {
+
+    @GET("{year}/{round}/results.json")
+    suspend fun results(
+        @Path("year") year: Int,
+        @Path("round") round: Int,
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int,
+    ): Response<BaseResponse<DataRaceTableDto>>
+
+    @GET("current/last/results.json")
+    suspend fun currentLastResults(): Response<BaseResponse<DataRaceTableDto>>
+
+}
