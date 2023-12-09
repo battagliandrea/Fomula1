faac8a9cfe18b1abf277ade8bc0af9674aa35de7
Andrea Battaglia <battaglia.andry@gmail.com>
2023-12-09 23:36:38 +0100

    feat: bind viewmodel to screen

--- feature/results/results-ui/src/main/kotlin/it/battagliandrea/formula1/feature/results/ui/ResultsScreen.kt
+++ feature/results/results-ui/src/main/kotlin/it/battagliandrea/formula1/feature/results/ui/ResultsScreen.kt
@@ -1,15 +1,28 @@
 package it.battagliandrea.formula1.feature.results.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import it.battagliandrea.formula1.domain.models.Result
 
 @Composable
-fun ResultsScreen() {
+fun ResultsScreen(
+    viewModel: ResultsViewModel = hiltViewModel(),
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -20,7 +33,29 @@ fun ResultsScreen() {
                 .fillMaxSize(),
             color = MaterialTheme.colorScheme.background,
         ) {
-            Text(text = "This is the results screen!")
+            when (val s = state) {
+                is ResultsUiState.Success -> {
+                    ResultList(results = s.races[0].results)
+                }
+                else -> {
+                    Text(text = "This is the results screen!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ResultList(
+    modifier: Modifier = Modifier,
+    results: List<Result>,
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp),
+    ) {
+        items(items = results) { result ->
+            Text(text = "${result.position} ${result.driver.code} ${result.time?.time.orEmpty()}")
         }
     }
 }
