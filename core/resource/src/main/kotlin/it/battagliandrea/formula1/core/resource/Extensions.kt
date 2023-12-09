f64cb9b2c83c6fd2000c2c6cacd609da344bdf84
Andrea Battaglia <battaglia.andry@gmail.com>
2023-12-09 23:49:23 +0100

    feat: add documentation

--- core/resource/src/main/kotlin/it/battagliandrea/formula1/core/resource/Extensions.kt
+++ core/resource/src/main/kotlin/it/battagliandrea/formula1/core/resource/Extensions.kt
@@ -20,6 +20,19 @@ inline fun <reified T> T.toResourceSuccess(): Resource<T> = Resource.Success(thi
  */
 inline fun <reified T> ErrorType.toResourceError(): Resource<T> = Resource.Error(this)
 
+/**
+ * Applies the given functions [isSuccess] and [isError] to the elements of the flow and
+ * returns a new flow of the results.
+ *
+ * This function is similar to the `fold` function on collections but operates on a flow of [Resource].
+ *
+ * @param isSuccess A suspend function to be applied if the resource is of type [Resource.Success].
+ *                  It takes the success value and returns the result.
+ * @param isError A suspend function to be applied if the resource is of type [Resource.Error].
+ *                It takes the error value and returns the result.
+ * @return A new flow containing the results of applying [isSuccess] to [Resource.Success] elements
+ *         and [isError] to [Resource.Error] elements.
+ */
 inline fun <T, R> Flow<Resource<T>>.fold(
     crossinline isSuccess: suspend (value: T) -> R,
     crossinline isError: suspend (value: ErrorType) -> R,
@@ -30,11 +43,29 @@ inline fun <T, R> Flow<Resource<T>>.fold(
     }
 }
 
+/**
+ * Transforms the success values of the given flow using the provided [transform] function.
+ *
+ * This function is similar to the `map` function on collections but operates on a flow of [Resource].
+ * Only the success values are transformed; error values are passed through unchanged.
+ *
+ * @param transform A suspend function to be applied to each success value, transforming it to a new value of type [O].
+ * @return A new flow containing the transformed success values while leaving error values unchanged.
+ */
 inline fun <I, O> Flow<Resource<I>>.mapSuccess(crossinline transform: suspend (value: I) -> O): Flow<Resource<O>> = transform { value ->
     require(value is Resource.Success)
     emit(Resource.Success(transform(value.data)))
 }
 
+/**
+ * Transforms the success values of the given flow using the provided [transform] function.
+ *
+ * This function is an extension of Flow<Resource<I>> and applies the [transform] function to each success value,
+ * producing a new flow of Resource<O>. Error values are passed through unchanged.
+ *
+ * @param transform A suspend function to be applied to each success value, transforming it to a new value of type [O].
+ * @return A new flow containing the transformed success values while leaving error values unchanged.
+ */
 inline fun <I> Flow<Resource<I>>.mapError(crossinline transform: suspend (value: ErrorType) -> ErrorType): Flow<Resource<I>> = transform { value ->
     require(value is Resource.Error)
     emit(Resource.Error(transform(value.error)))
--- feature/results/results-ui/src/main/kotlin/it/battagliandrea/formula1/feature/results/ui/ResultsUiState.kt
+++ feature/results/results-ui/src/main/kotlin/it/battagliandrea/formula1/feature/results/ui/ResultsUiState.kt
@@ -2,13 +2,27 @@ package it.battagliandrea.formula1.feature.results.ui
 
 import it.battagliandrea.formula1.domain.models.Race
 
+/**
+ * Sealed interface representing the different UI states for displaying race results.
+ */
 sealed interface ResultsUiState {
 
+    /**
+     * Represents the UI state when results are being loaded.
+     */
     data object Loading : ResultsUiState
 
+    /**
+     * Represents the UI state when results are successfully loaded.
+     *
+     * @property races A list of [Race] objects representing the loaded race results.
+     */
     data class Success(
         val races: List<Race>,
     ) : ResultsUiState
 
+    /**
+     * Represents the UI state when an error occurs during the loading of race results.
+     */
     data object Error : ResultsUiState
 }
