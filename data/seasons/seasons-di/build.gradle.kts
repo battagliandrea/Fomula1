7273ec489241371eaa8b3862c38e02f4f7f1a0b6
Andrea Battaglia <battaglia.andry@gmail.com>
2023-12-11 17:41:37 +0100

    feat: add seasons modules

---
+++ data/seasons/seasons-di/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("f1.android.library")
+    id("f1.android.hilt")
+}
+
+android {
+    namespace = "it.battagliandrea.formula1.data.seasons.di"
+}
+
+dependencies {
+    api(projects.data.seasons.seasonsApi)
+    api(projects.data.seasons.seasonsImpl)
+
+    implementation(projects.core.network.networkApi)
+    implementation(libs.kotlinx.serialization)
+}
