70605d124a74b38e63078d4fdd2c2c55dbaf3093
Andrea Battaglia <battaglia.andry@gmail.com>
2024-02-18 22:05:06 +0100

    feat: implement MainGraph

---
+++ feature/main/main-ui/src/main/kotlin/it/battagliandrea/formula1/feature/main/navigation/navigation/MainGraph.kt
@@ -0,0 +1,39 @@
+package it.battagliandrea.formula1.feature.main.navigation.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import it.battagliandrea.formula1.core.ui.navigation.popUpToTop
+import it.battagliandrea.formula1.feature.main.navigation.MainScreen
+
+const val MAIN_ROUTE_GRAPH = "main-graph"
+const val MAIN_ROUTE = "main"
+
+fun NavController.navigateToMain() {
+    navigate(MAIN_ROUTE)
+}
+
+fun NavController.navigateToMainGraph() {
+    navigate(route = MAIN_ROUTE_GRAPH) {
+        popUpToTop(this@navigateToMainGraph)
+    }
+}
+
+fun NavGraphBuilder.mainGraph(
+    builder: NavGraphBuilder.() -> Unit,
+) {
+    navigation(
+        route = MAIN_ROUTE_GRAPH,
+        startDestination = MAIN_ROUTE,
+        builder = builder,
+    )
+}
+
+fun NavGraphBuilder.mainScreen(
+    onNavigateBack: () -> Unit,
+) {
+    composable(route = MAIN_ROUTE) {
+        MainScreen()
+    }
+}
