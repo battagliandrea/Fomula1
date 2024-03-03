de74469af329ec656da1c6be229f3d23252d7c6f
Andrea Battaglia <battaglia.andry@gmail.com>
2024-03-03 15:01:18 +0100

    feat: move results use case to arrow

--- domain/usecase/src/main/kotlin/it/battagliandrea/formula1/domain/usecase/GetCurrentLastResultUseCase.kt
+++ domain/usecase/src/main/kotlin/it/battagliandrea/formula1/domain/usecase/GetCurrentLastResultUseCase.kt
@@ -1,17 +1,19 @@
 package it.battagliandrea.formula1.domain.usecase
 
+import arrow.core.Either
 import it.battagliandrea.formula1.data.results.api.repository.IResultsRepository
+import it.battagliandrea.formula1.domain.models.ErrorType
 import it.battagliandrea.formula1.domain.models.Race
 import it.battagliandrea.formula1.domain.usecase.GetCurrentLastResultUseCase.Params
-import it.battagliandrea.formula1.domain.usecase.internal.UseCase
+import it.battagliandrea.formula1.domain.usecase.internal.IFlowUseCase
 import javax.inject.Inject
 
 class GetCurrentLastResultUseCase @Inject constructor(
     private val resultsRepository: IResultsRepository,
-) : UseCase<Params, List<Race>> {
+) : IFlowUseCase<Params, Either<ErrorType, List<Race>>> {
 
     object Params
 
-    override fun invoke(params: Params) =
+    override fun execute(params: Params) =
         resultsRepository.getCurrentLastResult()
 }
--- domain/usecase/src/main/kotlin/it/battagliandrea/formula1/domain/usecase/GetResultsUseCase.kt
+++ domain/usecase/src/main/kotlin/it/battagliandrea/formula1/domain/usecase/GetResultsUseCase.kt
@@ -1,18 +1,20 @@
 package it.battagliandrea.formula1.domain.usecase
 
+import arrow.core.Either
 import it.battagliandrea.formula1.data.results.api.repository.IResultsRepository
+import it.battagliandrea.formula1.domain.models.ErrorType
 import it.battagliandrea.formula1.domain.models.Race
 import it.battagliandrea.formula1.domain.usecase.GetResultsUseCase.Params
-import it.battagliandrea.formula1.domain.usecase.internal.UseCase
+import it.battagliandrea.formula1.domain.usecase.internal.IFlowUseCase
 import javax.inject.Inject
 
 class GetResultsUseCase @Inject constructor(
     private val resultsRepository: IResultsRepository,
-) : UseCase<Params, List<Race>> {
+) : IFlowUseCase<Params, Either<ErrorType, List<Race>>> {
 
     data class Params(val year: Int, val round: Int)
 
-    override fun invoke(params: Params) =
+    override fun execute(params: Params) =
         resultsRepository.getResults(
             year = params.year,
             round = params.round,
