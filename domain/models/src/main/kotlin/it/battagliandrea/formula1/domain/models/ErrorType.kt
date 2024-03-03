2c0efe7c8973d25d1b6287f91a8b327fa245cc8d
Andrea Battaglia <battaglia.andry@gmail.com>
2024-03-03 15:00:42 +0100

    feat: add error models into domain level

---
+++ domain/models/src/main/kotlin/it/battagliandrea/formula1/domain/models/ErrorType.kt
@@ -0,0 +1,38 @@
+package it.battagliandrea.formula1.domain.models
+
+/**
+ * Sealed class representing different types of errors that can occur in the application.
+ */
+sealed class ErrorType {
+
+    /**
+     * Sealed class representing API-related errors.
+     */
+    sealed class Api : ErrorType() {
+
+        /**
+         * Represents a network-related error.
+         */
+        data object Network : Api()
+
+        /**
+         * Represents a service being unavailable error.
+         */
+        data object ServiceUnavailable : Api()
+
+        /**
+         * Represents a resource not found error.
+         */
+        data object NotFound : Api()
+
+        /**
+         * Represents a server-side error.
+         */
+        data object Server : Api()
+    }
+
+    /**
+     * Represents an unknown or generic error.
+     */
+    data object Unknown : ErrorType()
+}
