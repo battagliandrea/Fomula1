f0bc4d282f7f364e45707de10e563fe07339182b
Andrea Battaglia <battaglia.andry@gmail.com>
2024-03-04 23:24:20 +0100

    feat: improve domain models

--- domain/models/src/main/kotlin/it/battagliandrea/formula1/domain/models/Race.kt
+++ domain/models/src/main/kotlin/it/battagliandrea/formula1/domain/models/Race.kt
@@ -1,11 +1,13 @@
 package it.battagliandrea.formula1.domain.models
 
+import it.battagliandrea.formula1.domain.models.Schedule.*
+
 data class Race(
     val season: Long,
     val round: Int,
     val name: String,
     val circuit: Circuit,
-    val schedules: List<Schedule>,
+    val schedules: Map<SessionType, Schedule>,
     val results: List<Result> = emptyList(),
     val url: String = String(),
 )
