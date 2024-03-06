69980c1c317db89df55c8b31f3fecee25a6fde5f
Andrea Battaglia <battaglia.andry@gmail.com>
2024-03-06 10:58:24 +0100

    refactor: fix current last result use case

--- domain/usecase/src/main/kotlin/it/battagliandrea/formula1/domain/usecase/GetCurrentLastResultUseCase.kt
+++ domain/usecase/src/main/kotlin/it/battagliandrea/formula1/domain/usecase/GetCurrentLastResultUseCase.kt
@@ -3,17 +3,24 @@ package it.battagliandrea.formula1.domain.usecase
 import arrow.core.Either
 import it.battagliandrea.formula1.data.results.api.repository.IResultsRepository
 import it.battagliandrea.formula1.domain.models.ErrorType
-import it.battagliandrea.formula1.domain.models.Race
+import it.battagliandrea.formula1.domain.models.Result
 import it.battagliandrea.formula1.domain.usecase.GetCurrentLastResultUseCase.Params
 import it.battagliandrea.formula1.domain.usecase.internal.IFlowUseCase
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetCurrentLastResultUseCase @Inject constructor(
     private val resultsRepository: IResultsRepository,
-) : IFlowUseCase<Params, Either<ErrorType, List<Race>>> {
+) : IFlowUseCase<Params, Either<ErrorType, List<Result>>> {
 
     object Params
 
     override fun execute(params: Params) =
         resultsRepository.getCurrentLastResult()
+            .map { either ->
+                either.map { races ->
+                    races.firstOrNull()?.results.orEmpty()
+                }
+            }
 }
