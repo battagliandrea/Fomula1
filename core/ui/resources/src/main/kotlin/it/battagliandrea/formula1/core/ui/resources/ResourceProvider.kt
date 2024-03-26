1426c7d8ec29508e0fce40c35190614628ca4fa9
Andrea Battaglia <battaglia.andry@gmail.com>
2024-03-26 22:31:22 +0100

    fix: add placeholder to avatar image

--- core/ui/resources/src/main/kotlin/it/battagliandrea/formula1/core/ui/resources/ResourceProvider.kt
+++ core/ui/resources/src/main/kotlin/it/battagliandrea/formula1/core/ui/resources/ResourceProvider.kt
@@ -2,10 +2,10 @@ package it.battagliandrea.formula1.core.ui.resources
 
 object ResourceProvider {
 
-    fun getDriverAvatarByName(driverId: String) = when (driverId) {
+    fun getDriverAvatarByName(driverId: String): Int = when (driverId) {
         "max_verstappen" -> R.drawable.verstappen
         "perez" -> R.drawable.perez
         "leclerc" -> R.drawable.leclerc
-        else -> 0
+        else -> R.drawable.placeholder_driver
     }
 }
